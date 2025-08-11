440889f7e8658862bc31e880cc84e2cf49b67c30
Kyle Harris <87076483+RazorK1@users.noreply.github.com>
2025-08-11 15:50:11 -0400

    Update bu_bubbles.js

--- bu_bubbles.js
+++ bu_bubbles.js
@@ -71,7 +71,6 @@ bubble.prototype.moveBubble = function(height, width) {
 
 
 
-
 window.addEventListener("load", function() {
    // Reference to the bubble box
    var bubbleBox = document.getElementById("bubbleBox");
